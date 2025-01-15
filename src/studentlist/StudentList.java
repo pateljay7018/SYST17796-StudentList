3a5d37bb81c6b3edf14079c712e221ea2b6f8f8d
Jay Patel <pajayarv@sheridancollege.ca>
2025-01-15 10:14:19 -0500

    StudentList project version 3 - update array to 4 in student list size

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -15,7 +15,7 @@ public class StudentList {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Student[] studentList = new Student[3];//change size before running
+        Student[] studentList = new Student[4];//change size before running
         
         Student s1 = new Student();
         s1.setName("Fagun Vankawala");
@@ -25,12 +25,14 @@ public class StudentList {
         
         Student s3 = new Student();
         s3.setName("Megh Vankawala");
-//        Student s4 = new Student();
-//        s4.setName("Megh Vankawala");
+
+        Student s4 = new Student();
+        s4.setName("Jay Patel");
 //       
         studentList[0] = s1;
         studentList[1] = s2;
         studentList[2] = s3;
+        studentList[3] = s4;
         
         //fill in 4 student names before running
         for (Student s : studentList) {
